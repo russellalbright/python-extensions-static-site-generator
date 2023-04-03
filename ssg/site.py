202543f9b9f3da5149ea444156647f6012b6d84d
Russell Albright <russell.albright@sas.com>
2023-04-03 11:27:12 -0400

    Completed module

--- ssg/site.py
+++ ssg/site.py
@@ -1,4 +1,4 @@
-from ssg import extensions
+from ssg import extensions, hooks
 import sys
 from pathlib import Path
 
@@ -29,6 +29,7 @@ class Site:
 
     def build(self):
         extensions.load_bundled()
+        hooks.event(collect_files,self.source,self.parsers)
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
